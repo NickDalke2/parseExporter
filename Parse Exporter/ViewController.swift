79ebd23c64ba7b575e22341a099f72e87b88ecff
NickDalke2 <bagleymanor@gmail.com>
2017-01-12 16:32:52 -0800

    Initial Commit

---
+++ Parse Exporter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Parse Exporter
+//
+//  Created by Nick Dalke on 1/12/17.
+//  Copyright © 2017 Nick Dalke. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
